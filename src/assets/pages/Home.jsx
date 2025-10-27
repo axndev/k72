ca1cc89237aa69c0ec8147c61ea6909715926ac7
Kaleemullah <kaleemullahahsan0@gmail.com>
2025-10-27 20:23:36 +0500

    Homepage Responsive

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -5,7 +5,7 @@ import HeroBottom from '../components/Home/HeroBottom'
 function Home() {
     return (
         <>
-            <div className='relative'>
+            <div className='relative min-h-screen flex flex-col justify-center items-center'>
                 <HeroTop />
                 <p className="font-[font2] mb-5 p-5  pt-0 text-white max-w-100 ml-auto">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;K72 est une agence qui pense chaque action pour nourrir la marque. Demain, dans 5 mois et dans 5 ans. On cherche la friction qui crée l’étincelle pour générer de l’émotion. Pour assurer une relation honnête, on est sans filtre, on dit ce qui doit être dit, on fait ce qui doit être fait.</p>
                 <HeroBottom />
