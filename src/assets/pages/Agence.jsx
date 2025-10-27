dd6fa56251a5f5c65a3a90b7a703f67c7cfc4911
Kaleemullah <kaleemullahahsan0@gmail.com>
2025-10-27 20:45:56 +0500

    Agenece Page Responsive

--- src/assets/pages/Agence.jsx
+++ src/assets/pages/Agence.jsx
@@ -45,9 +45,9 @@ function Agence() {
       <div ref={imageDiv} className='absolute rounded-2xl overflow-hidden w-[18vw] top-40 left-[30vw]'>
         <img ref={imageCurrent} src="../../../public/images/1.jpg" alt="" />
       </div>
-      <div className='font-[font1] mt-[50vh] relative pb-80'>
+      <div className='font-[font1] md:mt-[50vh] mt-[30vh] relative pb-80'>
         <h1 className='font-[font2] uppercase text-[19vw] text-center leading-[17vw]'>Soixan7e Douze</h1>
-        <div className='text-6xl pl-[40vw]'>
+        <div className='md:text-6xl md:pl-[40vw] p-3'>
           <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
         </div>
       </div>
